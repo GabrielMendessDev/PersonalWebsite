009e945e614b040063076ca4bc51ec596528cd0c
Gabriel Mendes <lucasgabriel16161@gmail.com>
2024-06-03 13:29:32 -0300

    feat: modo dark blue

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -10,9 +10,9 @@ export default{
         buttonText: '#fff',
         
 
-        shadow: '0 2px 24px rgb(57 246 235 / 42%)',
+        shadow: '0 2px 24px rgb(57 23 235 / 42%)',
 
-        backgroundpresentation: 'radial-gradient(46.97% 53.54% at 50% 46.46%, rgb(57 246 235 / 42%) 0%, rgba(0, 1, 2, 0) 100%), #232532',
-        backgroundpresentation780: 'radial-gradient(97% 53.54% at 50% 46.46%, rgb(57 246 235 / 42%) 0%, rgba(0, 1, 2, 0) 100%), linear-gradient(0deg, #232532, #232532), rgb(255, 255, 255)',
+        backgroundpresentation: 'radial-gradient(46.97% 53.54% at 50% 46.46%, rgb(57 23 235 / 42%) 0%, rgba(0, 1, 2, 0) 100%), #232532',
+        backgroundpresentation780: 'radial-gradient(97% 53.54% at 50% 46.46%, rgb(57 23 235 / 42%) 0%, rgba(0, 1, 2, 0) 100%), linear-gradient(0deg, #232532, #232532), rgb(255, 255, 255)',
     }
 }
