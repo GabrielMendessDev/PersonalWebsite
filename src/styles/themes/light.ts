233aecce161bc348360bb87fa743144e49a2df88
Gabriel Mendes <lucasgabriel16161@gmail.com>
2024-06-11 23:26:35 -0300

    feat: adicionando o modo light

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -11,8 +11,5 @@ export default{
 
 
         shadow: '0 2px 24px rgb(57 140 235 / 42%)',
-
-        backgroundpresentation: 'radial-gradient(46.97% 53.54% at 50% 46.46%, rgb(57 140 235 / 42%) 0%, rgba(0, 1, 2, 0) 100%), #232532',
-        backgroundpresentation780: 'radial-gradient(97% 53.54% at 50% 46.46%, rgb(57 140 235 / 42%) 0%, rgba(0, 1, 2, 0) 100%), linear-gradient(0deg, #232532, #232532), rgb(255, 255, 255)',
     }
 }
